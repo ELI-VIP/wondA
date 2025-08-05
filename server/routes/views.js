bd69a8df1ef96c06817285de965d71adf8c7ad39
ELI-VIP <elijahokojie0@gmail.com>
2025-08-05 10:23:15 +0000

    Fresh deploy setup

--- server/routes/views.js
+++ server/routes/views.js
@@ -8,8 +8,8 @@ const __dirname = path.dirname(__filename);
 
 const base = path.join(__dirname, '..', '..', 'public', 'html');
 
-router.get('/', (req, res) => res.sendFile(path.join(base, 'home.html')));
-router.get('/home', (req, res) => res.sendFile(path.join(base, 'home.html')));
+router.get('/', (req, res) => res.sendFile(path.join(base, 'index.html')));
+router.get('/index', (req, res) => res.sendFile(path.join(base, 'index.html')));
 router.get('/login', (req, res) => res.sendFile(path.join(base, 'login.html')));
 router.get('/dashboard', (req, res) => res.sendFile(path.join(base, 'dashboard.html')));
 router.get('/register', (req, res) => res.sendFile(path.join(base, 'register.html')));
